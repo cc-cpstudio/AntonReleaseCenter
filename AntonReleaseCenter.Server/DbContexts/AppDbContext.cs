47ce40fd8bce3030829517fe0d77dac7ba05d1a7
programmer_cc <2140130341@qq.com>
2026-07-20 11:58:26 +0800

    feat 添加数据库连接

---
+++ AntonReleaseCenter.Server/DbContexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+using AntonReleaseCenter.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntonReleaseCenter.Server.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Channel> Channels { get; set; }
+    public DbSet<Software> Software { get; set; }
+    public DbSet<SoftwareRelease>  SoftwareReleases { get; set; }
+}
