6596de3c0191943b723f9bb252058d6a08d85e01
programmer_cc <2140130341@qq.com>
2026-07-20 14:22:23 +0800

    fix AppDbContext 未添加构造函数

--- AntonReleaseCenter.Server/DbContexts/AppDbContext.cs
+++ AntonReleaseCenter.Server/DbContexts/AppDbContext.cs
@@ -5,6 +5,8 @@ namespace AntonReleaseCenter.Server.DbContexts;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     public DbSet<Admin> Admins { get; set; }
     public DbSet<Channel> Channels { get; set; }
     public DbSet<Software> Software { get; set; }
