ece249d230a15ee5cd745452e0fb684a41f027de
programmer_cc <2140130341@qq.com>
2026-07-21 19:40:34 +0800

    feat AdminController

--- AntonReleaseCenter.Server/Extensions/ServiceCollectionExtensions.cs
+++ AntonReleaseCenter.Server/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddReleaseCenterServices(this IServiceCollection services)
     {
         services.AddScoped<IReleaseService, ReleaseService>();
+        services.AddScoped<IAdminService, AdminService>();
         return services;
     }
 }
