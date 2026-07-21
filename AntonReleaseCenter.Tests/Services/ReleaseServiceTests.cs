a46e6eda0d0bedd8c6e38d68be23ebc6b44c6cc7
programmer_cc <2140130341@qq.com>
2026-07-21 14:50:18 +0800

    feat SoftwareRelease Platform 属性

--- AntonReleaseCenter.Tests/Services/ReleaseServiceTests.cs
+++ AntonReleaseCenter.Tests/Services/ReleaseServiceTests.cs
@@ -162,7 +162,7 @@ public class ReleaseServiceTests
         var version = new Version(1, 0, 0, 0);
 
         var release = await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, version,
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, version,
             "First release", "/path/file.zip", 1024, "abc123",
             false, true));
 
@@ -181,7 +181,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Stable", 100));
         var release = await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(1, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(1, 0, 0, 0),
             "log", "/path.zip", 1024, "hash", false, true));
 
         var toggled = await service.ToggleReleaseOnlineAsync(release.SoftwareReleaseId);
@@ -228,7 +228,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Stable", 100));
         await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(2, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(2, 0, 0, 0),
             "offline", "/path.zip", 1024, "hash", false, false));
 
         var result = await service.CheckUpdateAsync("key", 1, new Version(1, 0, 0, 0), null);
@@ -246,7 +246,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Stable", 100));
         await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(1, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(1, 0, 0, 0),
             "latest", "/path.zip", 1024, "hash", false, true));
 
         var result = await service.CheckUpdateAsync("key", 1, new Version(1, 0, 0, 0), null);
@@ -264,7 +264,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Stable", 100));
         await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(2, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(2, 0, 0, 0),
             "New version!", "/path/v2.zip", 2048, "hash2", true, true));
 
         var result = await service.CheckUpdateAsync("key", 1, new Version(1, 0, 0, 0), null);
@@ -285,7 +285,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Beta", 50));
         await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(2, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(2, 0, 0, 0),
             "beta", "/path.zip", 1024, "hash", false, true));
 
         var deviceId = "device-in-range";
@@ -311,7 +311,7 @@ public class ReleaseServiceTests
         var channel = await service.CreateChannelAsync(software.SoftwareId,
             new CreateChannelRequest(1, "Stable", 100));
         await service.CreateReleaseAsync(new CreateReleaseRequest(
-            software.SoftwareId, channel.ChannelId, new Version(3, 0, 0, 0),
+            software.SoftwareId, channel.ChannelId, PlatformEnum.Windows_x64, new Version(3, 0, 0, 0),
             "forced update", "/path.zip", 1024, "hash", true, true));
 
         var result = await service.CheckUpdateAsync("key", 1, new Version(1, 0, 0, 0), null);
